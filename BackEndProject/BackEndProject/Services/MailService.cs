5a3a71105504d1a58e211d71fa501a1c7429f5d6
Elnur <elnurmj@code.edu.az>
2022-04-16 18:45:21 +0400

    Mail sent exception handled

--- BackEndProject/BackEndProject/Services/MailService.cs
+++ BackEndProject/BackEndProject/Services/MailService.cs
@@ -29,11 +29,14 @@ namespace BackEndProject.Services
 
             builder.HtmlBody = mailRequest.Body;
             email.Body = builder.ToMessageBody();
-            using var smtp = new SmtpClient();
-            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
-            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
-            await smtp.SendAsync(email);
-            smtp.Disconnect(true);
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+            }
+           
         }
     }
 }
